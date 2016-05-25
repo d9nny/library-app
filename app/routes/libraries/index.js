e538a7b5875687a4626359569aa72df0b0d9dc49
danwheeler <da-wheeler@hotmail.co.uk>
2016-05-25 23:17:17 +0100

    new library and view all added and complete

--- app/routes/libraries/index.js
+++ app/routes/libraries/index.js
@@ -2,7 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
 
-	model() {
-		return this.store.findAll('invitation');
-	}
+  model() {
+    return this.store.findAll('library');
+  }
+
 });
